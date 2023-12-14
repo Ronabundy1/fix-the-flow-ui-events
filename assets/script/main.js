e4d027c996ec6c63f650b0bb1ef3a9bd31acb1d1
Ronabundy1 <s1185083@student.windesheim.nl>
2023-12-14 20:04:41 +0100

    aanpassingen + interactie turn

--- assets/script/main.js
+++ assets/script/main.js
@@ -15,20 +15,30 @@ frontend.addEventListener("dblclick", function () {
 });
 
 // Verberg de 'Design'-link na klikken, komt weer in beeld in 3 seconden//
-const designLink = document.getElementById('design');
+const design = document.getElementById('design');
 
-designLink.addEventListener('click', function(event) {
-    designLink.classList.add('hidden'); // Verberg de 'Design'-link na klikken
+design.addEventListener('click', function(event) {
+    design.classList.add('hidden'); // Verberg de 'Design'-link na klikken
 
     // Toon de 'Design'-link weer na 3 seconden/3000 staat voor 3 seconden
     setTimeout(function() {
-        designLink.classList.remove('hidden');
+        design.classList.remove('hidden');
     }, 3000); 
 });
 document.addEventListener("DOMContentLoaded", function() {
-  const andLink = document.getElementById("and");
+  const and = document.getElementById("and");
 
-  andLink.addEventListener("dblclick", function(event) {
+  and.addEventListener("dblclick", function(event) {
     document.body.style.backgroundColor = "lightgreen"; //verander de achtergrondkleur
   });
 });
+
+const development = document.getElementById('development');
+
+development.addEventListener('animationend', turn);
+development.addEventListener('click', turn);
+
+function turn() {
+  development.classList.toggle('rotate'); 
+}
+
