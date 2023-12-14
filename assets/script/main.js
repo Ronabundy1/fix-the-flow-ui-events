6e1e3c80f7291077d4710f0566e7f396428cdeea
Ronabundy1 <s1185083@student.windesheim.nl>
2023-12-14 18:35:00 +0100

    interactie verberg knop na klik

--- assets/script/main.js
+++ assets/script/main.js
@@ -13,3 +13,16 @@ const frontend = document.getElementById('frontend');
 frontend.addEventListener("dblclick", function () {
   frontend.classList.toggle("changeColor");
 });
+
+// Verberg de 'Design'-link na klikken, komt weer in beeld in 3 seconden//
+const designLink = document.getElementById('design');
+
+designLink.addEventListener('click', function(event) {
+    designLink.classList.add('hidden'); // Verberg de 'Design'-link na klikken
+
+    // Toon de 'Design'-link weer na 3 seconden/3000 staat voor 3 seconden
+    setTimeout(function() {
+        designLink.classList.remove('hidden');
+    }, 3000); 
+});
+
